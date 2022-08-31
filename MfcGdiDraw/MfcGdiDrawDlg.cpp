ee836769f59e5a865d8d9d0ab312805e7a9af05c
HFラボ <hf.labo.free@gmail.com>
2022-08-31 11:47:30 +0900

    Initial commit

---
+++ MfcGdiDraw/MfcGdiDrawDlg.cpp
@@ -0,0 +1,64 @@
+﻿#include "pch.h"
+#include "framework.h"
+#include "MfcGdiDraw.h"
+#include "MfcGdiDrawDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CMfcGdiDrawDlg::CMfcGdiDrawDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCGDIDRAW_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMfcGdiDrawDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BOOL CMfcGdiDrawDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	SetIcon(m_hIcon, TRUE);
+	SetIcon(m_hIcon, FALSE);
+
+	return TRUE;
+}
+
+BEGIN_MESSAGE_MAP(CMfcGdiDrawDlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+void CMfcGdiDrawDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this);
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+HCURSOR CMfcGdiDrawDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
---
+++ MfcGdiDraw/MfcGdiDrawDlg.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class CMfcGdiDrawDlg : public CDialogEx
+{
+public:
+	CMfcGdiDrawDlg(CWnd* pParent = nullptr);
+
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCGDIDRAW_DIALOG };
+#endif
+
+protected:
+	HICON m_hIcon;
+
+	virtual void DoDataExchange(CDataExchange* pDX);
+	virtual BOOL OnInitDialog();
+
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
